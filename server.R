7cb8407d06e0da95a67f85bc57fc20fb084b8e9c
Kevin D'Elia <kevindelia@verizon.net>
2016-11-19 19:12:12 -0500

    Lots of UI and Pitch code changes to finalize the project.

--- server.R
+++ server.R
@@ -1,16 +1,22 @@
 library(shiny)
+
 shinyServer(function(input, output) {
   
+  # generate the models
   model.1 <- lm(Fertility ~ Agriculture, data = swiss)
   model.2 <- lm(Fertility ~ . -Agriculture, data = swiss)
+  
+  # create a factor vector for use in coloring plot
   faith <- ifelse(swiss$Catholic > 50.0, "Catholic", "Protestant")
   faiths <- as.factor(faith)
 
+  # use model 1 for predictions based on a single exploratory variable
   model1pred <- reactive({
     agricultureInput <- input$sliderAgriculture
     round(predict(model.1, newdata = data.frame(Agriculture = agricultureInput)), 2)
   })
   
+  # use model 2 for predictions based on multiple exploratory variables
   model2pred <- reactive({
     agricultureInput <- input$sliderAgriculture
     examinationInput <- input$sliderExamination
@@ -26,13 +32,17 @@ shinyServer(function(input, output) {
       )), 2)
   })
   
+  # send the plot to the UI
   output$plot1 <- renderPlot({
+    
+    # read the slider inputs
     agricultureInput <- input$sliderAgriculture
     examinationInput <- input$sliderExamination
     educationInput <- input$sliderEducation
     catholicInput <- input$sliderCatholic
     infantMortalityInput <- input$sliderInfantMortality
     
+    # create scatterplot of raw Fertility data
     plot(1:nrow(swiss), 
          swiss$Fertility, 
          xlab = "", 
@@ -43,32 +53,57 @@ shinyServer(function(input, output) {
          xlim = c(0, 100),
          ylim = c(0, 100))
 
+    # regression line for model 1
     if(input$showModel1){
-      abline(model.1, col = "green", lwd = 2)
+      abline(model.1, col = "blue", lwd = 2)
     }
     
+    # regression line for model 2
     if(input$showModel2){
       abline(model.2$coefficients[1:2], col = "orange", lwd = 2)
     }
     
-    legend(70, 100, c("Model 1 Prediction", "Model 2 Prediction"), pch = 16, 
-           col = c("green", "orange"), bty = "n", cex = 1.2)
+    # model information
+    legend(70, 
+           100, 
+           c("Model 1 Prediction", "Model 2 Prediction"), 
+           pch = 16, 
+           col = c("blue", "orange"), 
+           bty = "n", 
+           cex = 1.2)
+
+    # values of model predictions that change based on slider adjustments    
+    points(agricultureInput, model1pred(), col = "deepskyblue2", pch = 16, cex = 2)
+    points(examinationInput, model2pred(), col = "ivory4", pch = 15, cex = 2)
+    points(educationInput, model1pred(), col = "ivory4", pch = 16, cex = 2)
+    points(catholicInput, model1pred(), col = "ivory4", pch = 17, cex = 2)
+    points(infantMortalityInput, model1pred(), col = "ivory4", pch = 18, cex = 2)
     
-    points(agricultureInput, model1pred(), col = "green", pch = 16, cex = 2)
-    
-    points(examinationInput, model2pred(), col = "orange", pch = 16, cex = 2)
+    legend(5, 
+           40, 
+           c("Agriculture", "Examination", "Education", "Religion", "Infant Mortality"), 
+           pch = c(16, 15, 16, 17, 18), 
+           col = c("deepskyblue2", "ivory4", "ivory4", "ivory4", "ivory4"), 
+           bty = "n", 
+           cex = 1.2)    
   })
   
+  # formula for model 1
   output$formula1 <- renderText({
     deparse(model.1$call)
   })
+  
+  # output from model 1
   output$pred1 <- renderText({
     model1pred()
   })
   
+  # formula for model 2
   output$formula2 <- renderText({
     deparse(model.2$call)
   })
+  
+  # output from model 2
   output$pred2 <- renderText({
     model2pred()
   })
--- ui.R
+++ ui.R
@@ -1,39 +1,103 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  
   titlePanel("Predict Fertility Rates using two different models"),
   
   sidebarLayout(
-    
     sidebarPanel(
+      h4("Exploratory Variable for Model 1"),
       
-      sliderInput("sliderAgriculture", "% of males involved in agriculture as occupation", 1, 90, step = 0.5, value = 45),
+      sliderInput(
+        "sliderAgriculture",
+        "% of males involved in agriculture as occupation",
+        1,
+        90,
+        step = 0.5,
+        value = 45
+      ),
       
-      sliderInput("sliderExamination", "% draftees receiving highest mark on army examination", 3, 35, step = 1, value = 16),
+      br(),
       
-      sliderInput("sliderEducation", "% education beyond primary school for draftees", 1, 55, step = 1, value = 27),
+      h4("Exploratory Variables for Model 2"),
       
-      sliderInput("sliderCatholic", "% Catholic (as opposed to Protestant)", 2, 100, step = 0.5, value = 48),
+      sliderInput(
+        "sliderExamination",
+        "% draftees receiving highest mark on army examination",
+        3,
+        35,
+        step = 1,
+        value = 16
+      ),
       
-      sliderInput("sliderInfantMortality", "% live births who live less than 1 year", 10, 30, step = 0.5, value = 15),
+      sliderInput(
+        "sliderEducation",
+        "% education beyond primary school for draftees",
+        1,
+        55,
+        step = 1,
+        value = 27
+      ),
+      
+      sliderInput(
+        "sliderCatholic",
+        "% Catholic (as opposed to Protestant)",
+        2,
+        100,
+        step = 0.5,
+        value = 48
+      ),
+      
+      sliderInput(
+        "sliderInfantMortality",
+        "% live births who live less than 1 year",
+        10,
+        30,
+        step = 0.5,
+        value = 15
+      ),
+      
+      br(),
       
       checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
       
-      checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE)
+      checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE),
+      
+      br(),
+      
+      submitButton("Generate Models")
     ),
     
-    mainPanel(
+    mainPanel(tabsetPanel(
+      tabPanel(
+        "Plot",
+        plotOutput("plot1"),
+        h3("Predicted Fertility from Model 1:"),
+        h4(textOutput("formula1")),
+        h5(textOutput("pred1")),
+        br(),
+        h3("Predicted Fertility from Model 2:"),
+        h4(textOutput("formula2")),
+        h5(textOutput("pred2"))
+      ),
+      tabPanel("Help",
+               h2("How to use the application"),
+               h3("Slider bars"),
+               tags$ol(
+                 tags$li("The top slider controls the input to the first model."), 
+                 tags$li("The next four sliders control the inputs to the second model."), 
+                 tags$li("The checkboxes control the display of the regression line."),
+                 tags$li("The pushbutton runs the models with the selected paramters.")
+               ),
+               br(),
+               helpText("Once the pushbutton is pressed, the results of running the models (i.e., the predicted"),
+               helpText(" Fertility rate) will displayed graphically in the plot and textually below the plot."),
+               br(),
+               helpText("The various icons described in the lower left-hand legend will"),
+               helpText("adjust accordingly based on the inputs and model calculations."),
+               br(),
+               helpText("The regression lines will remain unchanged.")
+               )
       
-      plotOutput("plot1"),
-      
-      h3("Predicted Fertility from Model 1:"),
-      h4(textOutput("formula1")),
-      h5(textOutput("pred1")),
-      
-      h3("Predicted Fertility from Model 2:"),
-      h4(textOutput("formula2")),
-      h5(textOutput("pred2"))
-    )
+    ))
   )
 ))
